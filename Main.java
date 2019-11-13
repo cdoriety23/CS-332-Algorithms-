1c21dd0aacd629c986b04c3d69f7d55a0fac31c1
Chris D <49670599+cdoriety23@users.noreply.github.com>
2019-11-13 10:33:42 -0600

    Fibonacci sequence using dynamic programming

---
+++ Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.HashMap;
+
+//fibanocci using dynamic programming
+public class Main {
+
+    public static void main(String[] args) {
+	 System.out.println(fib(5));// should return 5
+        System.out.println(fib(6)); //should return 8
+        System.out.println(fib(10));// should return 55
+    }
+
+    public static HashMap<Integer, Integer> num = new HashMap<Integer, Integer>();
+    public static int fib(int n) {
+        if(num.containsKey(n)) return num.get(n);
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        int Value = fib(n - 1) + fib(n - 2);
+        num.put(n, Value);
+        return Value;
+    }
+    {
+
+    }
+
+
+
+}
