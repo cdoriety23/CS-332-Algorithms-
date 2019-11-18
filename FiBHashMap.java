9d56705b7ad827fdc1fac673a6099decfd36d228
Chris D <49670599+cdoriety23@users.noreply.github.com>
2019-11-18 13:34:55 -0600

    Fibannoci using Hashmap, DP

--- FiBHashMap.java
+++ FiBHashMap.java
@@ -13,11 +13,21 @@ public class Main {
 
     public static HashMap<Integer, Integer> num = new HashMap<Integer, Integer>();
     public static int fib(int n) {
-        if(num.containsKey(n)) return num.get(n);
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        int Value = fib(n - 1) + fib(n - 2);
-        num.put(n, Value);
+        if(num.containsKey(n)) 
+	{
+		return num.get(n);
+	}
+        if(n == 0) 
+	{
+		return 0;
+	}
+        if(n == 1) 
+	{
+		return 1;
+	}
+        
+	    	int Value = fib(n - 1) + fib(n - 2);
+        	num.put(n, Value);
         return Value;
     }
     {
